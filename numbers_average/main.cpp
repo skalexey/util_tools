bd0acafc2b734af8847ef4407df0890adcc11299
skalexey <skorohodov-ad@narod.ru>
2022-06-15 12:45:06 +0300

    Automate inputting

--- numbers_average/main.cpp
+++ numbers_average/main.cpp
@@ -14,16 +14,27 @@ int main(int argc, char* argv[])
 {
 	cout << "=== numbers_average ===" << endl;
 
-	std::vector<std::string> v = {
-		"26/49/7/9	567"
-		,"15/65/7/7	654"
-		,"15/60/7/10	628"
-	};
+	std::vector<std::string> v;
+	int n = 1;
+	while (true)
+	{
+		cout << "Number list " << n++ << " (or type 'end' to stop entering): ";
+		std::string number_list;
+		std::getline(cin, number_list);
+		if (number_list == "end")
+			break;
+		v.push_back(number_list);
+	}
+	//std::vector<std::string> v = {
+	//	"26/49/7/9	567"
+	//	,"15/65/7/7	654"
+	//	,"15/60/7/10	628"
+	//};
 	std::vector<float> nums;
 	int i = 1;
 	for (auto&& e : v)
 	{
-		auto nums_s = utils::split_of(e, " \t/");
+		auto nums_s = utils::split_of(e, " \t,|/");
 		nums.resize(std::max(nums.size(), nums_s.size()));
 		std::transform(nums_s.begin(), nums_s.end(), nums.begin(), nums.begin(), [&](auto&& nums_s_e, auto&& nums_e) {
 			auto val = nums_e + std::stof(std::string(nums_s_e));
--- numbers_transform/main.cpp
+++ numbers_transform/main.cpp
@@ -14,12 +14,21 @@ int main(int argc, char* argv[])
 {
 	cout << "=== numbers_transofm ===" << endl;
 	
-	std::string s = "0.8/11/2.8/3/2	115";
-	auto nums_s = utils::split_of(s, " \t/");
+	cout << "Number list: ";
+	std::string number_list;
+	std::getline(cin, number_list);
+	auto v = utils::split_of(number_list, " \t,|/");
+	cout << "\n";
+	cout << "Multiplier: ";
+	float mpl = float();
+	cin >> mpl;
+	cout << "\n";
+	//std::string s = "0.8/11/2.8/3/2	115";
 	std::vector<float> nums;
-	std::transform(nums_s.begin(), nums_s.end(), std::back_inserter(nums), [](auto&& sv) {
-		return std::stof(std::string(sv)) * 0.825;
+	std::transform(v.begin(), v.end(), std::back_inserter(nums), [&](auto&& sv) {
+		return std::stof(std::string(sv)) * mpl;
 	});
+	cout << "Result: ";
 	std::copy(nums.begin(), nums.end(), std::ostream_iterator<float>(std::cout, " "));
 	std::cout << "\n";
 	return 0;
