2d0a66c77b960f4494b10d3bfe6ac3306894b67a
skalexey <skorohodov-ad@narod.ru>
2023-01-22 22:19:24 +0100

    Auto update build scripts

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -2,8 +2,9 @@
 
 function deps_scenario()
 {
-    source dependencies.sh
-    source deps_config.sh
+	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    source $THIS_DIR/dependencies.sh
+    source $THIS_DIR/deps_config.sh
 
     download_dependency "$vlDirRel" "$depsLocationFull" "git@github.com:skalexey/VL.git"
     download_dependency "$rapidjsonDirRel" "$depsLocationFull" "https://github.com/Tencent/rapidjson.git"
